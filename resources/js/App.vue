e55056fb745f1634fc9c9a77f4e4a825cf5b9419
Karol Kluczniok <karolkluczniok@MacBook-Pro-Karol.local>
2019-12-30 15:18:39 +0100

    setup

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <b-container>
+      <router-view/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Inconsolata&display=swap');
+
+* {
+  font-family: 'Inconsolata', monospace;
+}
+</style>
