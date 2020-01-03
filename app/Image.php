f8e607a71a1168484cfe035ad44dd5a211fc87f7
Karol Kluczniok <karolkluczniok@MacBook-Pro-Karol.local>
2020-01-03 22:18:26 +0100

    started Image backend

---
+++ app/Image.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->{$user->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
